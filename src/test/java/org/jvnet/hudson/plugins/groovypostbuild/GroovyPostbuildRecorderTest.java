c2dbef07110bb3532e18292c98ea38e4808162b6
ikedam <devld@ikedam.jp>
2015-08-16 11:34:18 +0900

    [JENKINS-23116] Added tests for behavior (Do nothing, Mark build as unstable, Mark build as failed).

--- src/test/java/org/jvnet/hudson/plugins/groovypostbuild/GroovyPostbuildRecorderTest.java
+++ src/test/java/org/jvnet/hudson/plugins/groovypostbuild/GroovyPostbuildRecorderTest.java
@@ -26,14 +26,18 @@ package org.jvnet.hudson.plugins.groovypostbuild;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import hudson.matrix.AxisList;
 import hudson.matrix.Combination;
 import hudson.matrix.MatrixBuild;
 import hudson.matrix.MatrixProject;
 import hudson.matrix.TextAxis;
+import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
+import hudson.model.Result;
 
 import org.apache.commons.lang.StringUtils;
 import org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.SecureGroovyScript;
@@ -41,10 +45,14 @@ import org.jenkinsci.plugins.scriptsecurity.scripts.ClasspathEntry;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.FailureBuilder;
+import org.jvnet.hudson.test.UnstableBuilder;
 
 public class GroovyPostbuildRecorderTest {
     @ClassRule
     public static JenkinsRule j = new JenkinsRule();
+    
+    private static final String TEXT_ON_FAILED = "Groovy";
     
     private static final String SCRIPT_FOR_MATRIX = StringUtils.join(new String[]{
             "import hudson.matrix.MatrixBuild;",
@@ -89,5 +97,345 @@ public class GroovyPostbuildRecorderTest {
         assertNull(b.getAction(GroovyPostbuildAction.class));
         assertEquals("value1", b.getRun(new Combination(axisList, "value1")).getAction(GroovyPostbuildAction.class).getText());
         assertEquals("value2", b.getRun(new Combination(axisList, "value2")).getAction(GroovyPostbuildAction.class).getText());
+    }
+    
+    /**
+     * behavior = any
+     * build succeeds
+     * script succeeds
+     * -> build succeeds
+     * @throws Exception
+     */
+    @Test
+    public void testBehaviorNotAffectWithSucceedingBuildSucceedingScript() throws Exception {
+        List<Integer> behaviors = Arrays.asList(0, 1, 2);
+        for (int behavior: behaviors) {
+            FreeStyleProject p = j.createFreeStyleProject();
+            
+            p.getPublishersList().add(new GroovyPostbuildRecorder(
+                    new SecureGroovyScript(
+                            "manager.addShortText('testing');",
+                            true,
+                            Collections.<ClasspathEntry>emptyList()
+                    ),
+                    behavior,   // behavior
+                    false       // runForMatrixParent
+            ));
+            
+            FreeStyleBuild b = p.scheduleBuild2(0).get();
+            j.assertBuildStatus(Result.SUCCESS, b);
+            assertEquals("testing", b.getAction(GroovyPostbuildAction.class).getText());
+        }
+    }
+    
+    /**
+     * behavior = any
+     * build unstable
+     * script succeeds
+     * -> build unstable
+     * @throws Exception
+     */
+    @Test
+    public void testBehaviorNotAffectWithUnstableBuildSuceedingScript() throws Exception {
+        List<Integer> behaviors = Arrays.asList(0, 1, 2);
+        for (int behavior: behaviors) {
+            FreeStyleProject p = j.createFreeStyleProject();
+            
+            p.getBuildersList().add(new UnstableBuilder());
+            
+            p.getPublishersList().add(new GroovyPostbuildRecorder(
+                    new SecureGroovyScript(
+                            "manager.addShortText('testing');",
+                            true,
+                            Collections.<ClasspathEntry>emptyList()
+                    ),
+                    behavior,   // behavior
+                    false       // runForMatrixParent
+            ));
+            
+            FreeStyleBuild b = p.scheduleBuild2(0).get();
+            j.assertBuildStatus(Result.UNSTABLE, b);
+            assertEquals("testing", b.getAction(GroovyPostbuildAction.class).getText());
+        }
+    }
+    
+    /**
+     * behavior = any
+     * build failed
+     * script succeeds
+     * -> build failed
+     * @throws Exception
+     */
+    @Test
+    public void testBehaviorNotAffectWithFailingBuildSuceedingScript() throws Exception {
+        List<Integer> behaviors = Arrays.asList(0, 1, 2);
+        for (int behavior: behaviors) {
+            FreeStyleProject p = j.createFreeStyleProject();
+            
+            p.getBuildersList().add(new FailureBuilder());
+            
+            p.getPublishersList().add(new GroovyPostbuildRecorder(
+                    new SecureGroovyScript(
+                            "manager.addShortText('testing');",
+                            true,
+                            Collections.<ClasspathEntry>emptyList()
+                    ),
+                    behavior,   // behavior
+                    false       // runForMatrixParent
+            ));
+            
+            FreeStyleBuild b = p.scheduleBuild2(0).get();
+            j.assertBuildStatus(Result.FAILURE, b);
+            assertEquals("testing", b.getAction(GroovyPostbuildAction.class).getText());
+        }
+    }
+    
+    /**
+     * behavior = DoNothing(0)
+     * build succeeds
+     * script failed
+     * -> build succeeds
+     * @throws Exception
+     */
+    @Test
+    public void testBehaviorDoNothingWithSucceedingBuildFailingScript() throws Exception {
+        FreeStyleProject p = j.createFreeStyleProject();
+        
+        p.getPublishersList().add(new GroovyPostbuildRecorder(
+                new SecureGroovyScript(
+                        "blahblahblah",
+                        true,
+                        Collections.<ClasspathEntry>emptyList()
+                ),
+                0,      // behavior
+                false   // runForMatrixParent
+        ));
+        
+        FreeStyleBuild b = p.scheduleBuild2(0).get();
+        j.assertBuildStatus(Result.SUCCESS, b);
+        assertEquals(TEXT_ON_FAILED, b.getAction(GroovyPostbuildAction.class).getText());
+    }
+    
+    
+    /**
+     * behavior = DoNothing(0)
+     * build unstable
+     * script failed
+     * -> build unstable
+     * @throws Exception
+     */
+    @Test
+    public void testBehaviorDoNothingWithUnstableBuildFailingScript() throws Exception {
+        FreeStyleProject p = j.createFreeStyleProject();
+        
+        p.getBuildersList().add(new UnstableBuilder());
+        
+        p.getPublishersList().add(new GroovyPostbuildRecorder(
+                new SecureGroovyScript(
+                        "blahblahblah",
+                        true,
+                        Collections.<ClasspathEntry>emptyList()
+                ),
+                0,      // behavior
+                false   // runForMatrixParent
+        ));
+        
+        FreeStyleBuild b = p.scheduleBuild2(0).get();
+        j.assertBuildStatus(Result.UNSTABLE, b);
+        assertEquals(TEXT_ON_FAILED, b.getAction(GroovyPostbuildAction.class).getText());
+    }
+    
+    /**
+     * behavior = DoNothing(0)
+     * build failed
+     * script failed
+     * -> build failed
+     * @throws Exception
+     */
+    @Test
+    public void testBehaviorDoNothingWithFailingBuildFailingScript() throws Exception {
+        FreeStyleProject p = j.createFreeStyleProject();
+        
+        p.getBuildersList().add(new FailureBuilder());
+        
+        p.getPublishersList().add(new GroovyPostbuildRecorder(
+                new SecureGroovyScript(
+                        "blahblahblah",
+                        true,
+                        Collections.<ClasspathEntry>emptyList()
+                ),
+                0,      // behavior
+                false   // runForMatrixParent
+        ));
+        
+        FreeStyleBuild b = p.scheduleBuild2(0).get();
+        j.assertBuildStatus(Result.FAILURE, b);
+        assertEquals(TEXT_ON_FAILED, b.getAction(GroovyPostbuildAction.class).getText());
+    }
+    
+    /**
+     * behavior = Mark build as unstable(1)
+     * build succeeds
+     * script failed
+     * -> build unstable
+     * @throws Exception
+     */
+    @Test
+    public void testBehaviorMarkUnstableWithSucceedingBuildFailingScript() throws Exception {
+        FreeStyleProject p = j.createFreeStyleProject();
+        
+        p.getPublishersList().add(new GroovyPostbuildRecorder(
+                new SecureGroovyScript(
+                        "blahblahblah",
+                        true,
+                        Collections.<ClasspathEntry>emptyList()
+                ),
+                1,      // behavior
+                false   // runForMatrixParent
+        ));
+        
+        FreeStyleBuild b = p.scheduleBuild2(0).get();
+        j.assertBuildStatus(Result.UNSTABLE, b);
+        assertEquals(TEXT_ON_FAILED, b.getAction(GroovyPostbuildAction.class).getText());
+    }
+    
+    
+    /**
+     * behavior = Mark build as unstable(1)
+     * build unstable
+     * script failed
+     * -> build unstable
+     * @throws Exception
+     */
+    @Test
+    public void testBehaviorMarkUnstableWithUnstableBuildFailingScript() throws Exception {
+        FreeStyleProject p = j.createFreeStyleProject();
+        
+        p.getBuildersList().add(new UnstableBuilder());
+        
+        p.getPublishersList().add(new GroovyPostbuildRecorder(
+                new SecureGroovyScript(
+                        "blahblahblah",
+                        true,
+                        Collections.<ClasspathEntry>emptyList()
+                ),
+                1,      // behavior
+                false   // runForMatrixParent
+        ));
+        
+        FreeStyleBuild b = p.scheduleBuild2(0).get();
+        j.assertBuildStatus(Result.UNSTABLE, b);
+        assertEquals(TEXT_ON_FAILED, b.getAction(GroovyPostbuildAction.class).getText());
+    }
+    
+    /**
+     * behavior = Mark build as unstable(1)
+     * build failed
+     * script failed
+     * -> build failed
+     * @throws Exception
+     */
+    @Test
+    public void testBehaviorMarkUnstableWithFailingBuildFailingScript() throws Exception {
+        FreeStyleProject p = j.createFreeStyleProject();
+        
+        p.getBuildersList().add(new FailureBuilder());
+        
+        p.getPublishersList().add(new GroovyPostbuildRecorder(
+                new SecureGroovyScript(
+                        "blahblahblah",
+                        true,
+                        Collections.<ClasspathEntry>emptyList()
+                ),
+                1,      // behavior
+                false   // runForMatrixParent
+        ));
+        
+        FreeStyleBuild b = p.scheduleBuild2(0).get();
+        j.assertBuildStatus(Result.FAILURE, b);
+        assertEquals(TEXT_ON_FAILED, b.getAction(GroovyPostbuildAction.class).getText());
+    }
+    
+    /**
+     * behavior = Mark build as failed (2)
+     * build succeeds
+     * script failed
+     * -> build failed
+     * @throws Exception
+     */
+    @Test
+    public void testBehaviorMarkFailedWithSucceedingBuildFailingScript() throws Exception {
+        FreeStyleProject p = j.createFreeStyleProject();
+        
+        p.getPublishersList().add(new GroovyPostbuildRecorder(
+                new SecureGroovyScript(
+                        "blahblahblah",
+                        true,
+                        Collections.<ClasspathEntry>emptyList()
+                ),
+                2,      // behavior
+                false   // runForMatrixParent
+        ));
+        
+        FreeStyleBuild b = p.scheduleBuild2(0).get();
+        j.assertBuildStatus(Result.FAILURE, b);
+        assertEquals(TEXT_ON_FAILED, b.getAction(GroovyPostbuildAction.class).getText());
+    }
+    
+    
+    /**
+     * behavior = Mark build as failed (2)
+     * build unstable
+     * script failed
+     * -> build failed
+     * @throws Exception
+     */
+    @Test
+    public void testBehaviorMarkFailedWithUnstableBuildFailingScript() throws Exception {
+        FreeStyleProject p = j.createFreeStyleProject();
+        
+        p.getBuildersList().add(new UnstableBuilder());
+        
+        p.getPublishersList().add(new GroovyPostbuildRecorder(
+                new SecureGroovyScript(
+                        "blahblahblah",
+                        true,
+                        Collections.<ClasspathEntry>emptyList()
+                ),
+                2,      // behavior
+                false   // runForMatrixParent
+        ));
+        
+        FreeStyleBuild b = p.scheduleBuild2(0).get();
+        j.assertBuildStatus(Result.FAILURE, b);
+        assertEquals(TEXT_ON_FAILED, b.getAction(GroovyPostbuildAction.class).getText());
+    }
+    
+    /**
+     * behavior = Mark build as failed (2)
+     * build failed
+     * script failed
+     * -> build failed
+     * @throws Exception
+     */
+    @Test
+    public void testBehaviorMarkFailedWithFailingBuildFailingScript() throws Exception {
+        FreeStyleProject p = j.createFreeStyleProject();
+        
+        p.getBuildersList().add(new FailureBuilder());
+        
+        p.getPublishersList().add(new GroovyPostbuildRecorder(
+                new SecureGroovyScript(
+                        "blahblahblah",
+                        true,
+                        Collections.<ClasspathEntry>emptyList()
+                ),
+                2,      // behavior
+                false   // runForMatrixParent
+        ));
+        
+        FreeStyleBuild b = p.scheduleBuild2(0).get();
+        j.assertBuildStatus(Result.FAILURE, b);
+        assertEquals(TEXT_ON_FAILED, b.getAction(GroovyPostbuildAction.class).getText());
     }
 }
