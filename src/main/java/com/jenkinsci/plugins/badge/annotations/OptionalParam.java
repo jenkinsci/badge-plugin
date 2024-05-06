ecb05d53bd1d9de2e673fee67c1322080eedc5b4
Kyle Cronin <cronik@users.noreply.github.com>
2024-05-06 11:01:21 -0400

    Remove name parameter from OptionalParam
    This addition is not needed after refactoring type hierarchy.

--- src/main/java/com/jenkinsci/plugins/badge/annotations/OptionalParam.java
+++ src/main/java/com/jenkinsci/plugins/badge/annotations/OptionalParam.java
@@ -38,12 +38,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 @Target({ElementType.METHOD, ElementType.PARAMETER})
 public @interface OptionalParam {
-
-  /**
-   * @return the name of this param
-   */
-  String name() default "";
-
+  
   /**
    * @return description for this param
    */
